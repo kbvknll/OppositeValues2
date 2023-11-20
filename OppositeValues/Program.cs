cd7fade4c37e8c0c1bb761c79796da4f860d9957
Екатерина Гордеева <katiyhagordeeva@mail.ru>
2023-11-20 10:27:06 +0300

    minor fix

--- OppositeValues/Program.cs
+++ OppositeValues/Program.cs
@@ -4,6 +4,7 @@ class Program
     static void Main()
     {
         Console.Write("Введите длину массива: ");
+        Console.Write("Введите длину массива: ");
         int n = Convert.ToInt32(Console.ReadLine());
         if (n < 0 || n > 100)
         {
